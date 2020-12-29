670e2cda8d779f53c2b754b5cff3bc1f6c62d1fe
Danel Polyakov <danelpo@gmail.com>
2020-12-29 14:55:04 -0500

    Added double click listener and selection reader
    -When double clicking on a word and the word get selected, it'll now pop
    it up in an alert.

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
 console.log("Wikipedia QuickSearch is running");
+
+//chrome.webNavigation.onCommitted.addEventListener('dblclick', function (e) {alert()})
+document.addEventListener('dblclick', function () {
+    alert(document.getSelection().getRangeAt(0).toString().trim());
+})
