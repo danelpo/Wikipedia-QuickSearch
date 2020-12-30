678e09d02eb40dfc0f82fdcfc204bc0a6b2ea7a0
Danel Polyakov <danelpo@gmail.com>
2020-12-29 23:00:36 -0500

    Wikipedia article summary in modal
    -the modal now shows a short wikipedia article summary (up to 700 char)
    -the modal disappears when screen is clicked

--- content.js
+++ content.js
@@ -1,13 +1,18 @@
-console.log("Wikipedia QuickSearch is running");
+const modalID = "myPopupModalID";
 
-//chrome.webNavigation.onCommitted.addEventListener('dblclick', function (e) {alert()})
 document.addEventListener('dblclick', function () {
     //need to verify that a STRING is selected (not pictures, documents...)
     if(document.getSelection().getRangeAt(0).toString().trim().length > 0){
         let searchWord = document.getSelection().getRangeAt(0).toString().trim();
         retriveSearch(searchWord);
     }
-})
+});
+
+document.addEventListener('click', function(){
+    if(document.getElementById(modalID)){
+        document.getElementById(modalID).remove();
+    }
+});
 
 function retriveSearch(searchWord){
     console.log();
@@ -33,8 +38,35 @@ function retriveSearch(searchWord){
         if (http.readyState == 4 && http.status == 200) {
             const myJSON = JSON.parse(http.responseText).query.pages;
             let articleExtract = myJSON[Object.keys(myJSON)[0]].extract;
-            console.log(articleExtract);
-
+            if(articleExtract){
+                if(articleExtract.length > 45){
+                    if(articleExtract.length > 700) {
+                        makePopup(articleExtract.substring(0, 700)+"...");
+                    }else{
+                        makePopup(articleExtract);
+                    }
+                }
+            }
         }
     }
+}
+
+function makePopup(article){
+    var modal = document.createElement('div');
+    modal.style.position = "fixed";
+    modal.style.bottom = "2%";
+    modal.style.right = "2%";
+    modal.style.width = "25%";
+    modal.style.height = "50%";
+    modal.style.zIndex = "99";
+    modal.style.backgroundColor = "white";
+    modal.style.border = "2px solid rgba(0,0,0,0.1)";
+    modal.innerHTML = article;
+    modal.id = modalID;
+    modal.style.fontFamily = 'font-family: "Times New Roman", Times, serif';
+    modal.style.fontSize = "12px";
+    modal.style.padding = "30px";
+    modal.style.borderRadius = "5%";
+
+    document.body.appendChild(modal);
 }
