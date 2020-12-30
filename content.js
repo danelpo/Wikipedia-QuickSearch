e8fbadccd53a5c96014cb0d2c4c1da6812c4037e
Danel Polyakov <danelpo@gmail.com>
2020-12-29 20:54:42 -0500

    Added wikipedia API in console
    -when a text is double clicked, it'll search the word in wikipedia and
    return the result in console.
    -added github link to button popup

--- content.js
+++ content.js
@@ -2,5 +2,39 @@ console.log("Wikipedia QuickSearch is running");
 
 //chrome.webNavigation.onCommitted.addEventListener('dblclick', function (e) {alert()})
 document.addEventListener('dblclick', function () {
-    alert(document.getSelection().getRangeAt(0).toString().trim());
+    //need to verify that a STRING is selected (not pictures, documents...)
+    if(document.getSelection().getRangeAt(0).toString().trim().length > 0){
+        let searchWord = document.getSelection().getRangeAt(0).toString().trim();
+        retriveSearch(searchWord);
+    }
 })
+
+function retriveSearch(searchWord){
+    console.log();
+    let article = searchWord;
+
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const url = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=${article}`;
+    const http = new XMLHttpRequest();
+
+    try {
+        if(window.location.hostname.slice(-13) != "wikipedia.org"){
+            http.open("GET", (proxyURL + url));
+        }else{
+            http.open("GET", url);
+        }
+        http.setRequestHeader('Access-Control-Allow-Origin', '*');
+        http.send();
+    } catch (error) {
+        console.log(error);
+    }
+
+    http.onreadystatechange=(e)=>{
+        if (http.readyState == 4 && http.status == 200) {
+            const myJSON = JSON.parse(http.responseText).query.pages;
+            let articleExtract = myJSON[Object.keys(myJSON)[0]].extract;
+            console.log(articleExtract);
+
+        }
+    }
+}
