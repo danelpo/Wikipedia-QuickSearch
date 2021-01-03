cbd4a9d73cce01eb5397f8067f1cc1c142810d16
bretwang <me@bretwang.com>
2021-01-03 19:02:15 +0900

    update for japanese

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 const modalID = "myPopupModalID";
 
 document.addEventListener('dblclick', function () {
-    if(document.getSelection().type == "Range"){//something is selected
-        if(typeof document.getSelection().focusNode.nodeValue == "string"){//a string is selected
-            if(document.getSelection().getRangeAt(0).toString().trim().length > 0){//the string is not empty or whitespace
+    if (document.getSelection().type == "Range") {//something is selected
+        if (typeof document.getSelection().focusNode.nodeValue == "string") {//a string is selected
+            if (document.getSelection().getRangeAt(0).toString().trim().length > 0) {//the string is not empty or whitespace
                 let searchWord = document.getSelection().getRangeAt(0).toString().trim();
                 retriveSearch(searchWord);
             }
@@ -11,24 +11,26 @@ document.addEventListener('dblclick', function () {
     }
 });
 
-document.addEventListener('click', function(e){
-    if(document.getElementById(modalID)){
+document.addEventListener('click', function (e) {
+    if (document.getElementById(modalID)) {
         document.getElementById(modalID).remove();
     }
 }, false);
 
-function retriveSearch(searchWord){
+function retriveSearch(searchWord) {
     console.log();
     let article = searchWord;
 
+    const articleLang = new RegExp('([^\x01-\x7E])+', 'g').exec(article) ? 'ja' : 'en';
+
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-    const url = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&explaintext=&titles=${article}`;
+    const url = `https://${articleLang}.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&explaintext=&titles=${article}`;
     const http = new XMLHttpRequest();
 
     try {
-        if(window.location.hostname.slice(-13) != "wikipedia.org"){
+        if (window.location.hostname.slice(-13) != "wikipedia.org") {
             http.open("GET", (proxyURL + url));
-        }else{
+        } else {
             http.open("GET", url);
         }
         http.setRequestHeader('Access-Control-Allow-Origin', '*');
@@ -37,26 +39,21 @@ function retriveSearch(searchWord){
         console.log(error);
     }
 
-    http.onreadystatechange=(e)=>{
+    http.onreadystatechange = (e) => {
         if (http.readyState == 4 && http.status == 200) {
             const myJSON = JSON.parse(http.responseText).query.pages;
-            const articleExtract = myJSON[Object.keys(myJSON)[0]].extract;
+            const articleExtract = myJSON[Object.keys(myJSON)[0]].extract || '';
             const articleTitle = myJSON[Object.keys(myJSON)[0]].title;
 
-            if(articleExtract){
-                if(articleExtract.length > 45){
-                    if(articleExtract.length > 650) {
-                        makePopup(articleExtract.substring(0, 650)+"...", articleTitle);
-                    }else{
-                        makePopup(articleExtract, articleTitle);
-                    }
-                }
+            if (articleExtract.length > 650) {
+                articleExtract = articleExtract.substring(0, 650) + "..."
             }
+            makePopup(articleExtract, articleTitle, articleLang);
         }
     }
 }
 
-function makePopup(articleExtract, articleTitle){
+function makePopup(articleExtract, articleTitle, articleLang) {
     let modal = document.createElement('div');
     modal.style.position = "fixed";
     modal.style.bottom = "2%";
@@ -72,57 +69,58 @@ function makePopup(articleExtract, articleTitle){
     modal.style.borderRadius = "5%";
     modal.style.overflow = "auto";
 
-    modal.addEventListener('click', function(ev){
+    modal.addEventListener('click', function (ev) {
         ev.stopPropagation();
     }, false);
 
-        let title = document.createElement('h1');
-        title.innerHTML = articleTitle;
-        title.style = "font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; color: black; " +
-                    "font-weight: 600; font-size: 32px; padding: 0px; margin: 0px";
-        modal.appendChild(title);
+    let title = document.createElement('h1');
+    title.innerHTML = articleTitle;
+    title.style = "font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; color: black; " +
+        "font-weight: 600; font-size: 32px; padding: 0px; margin: 0px";
+    modal.appendChild(title);
 
-        let imageLink = document.createElement('a');
-        imageLink.href = "https://en.wikipedia.org";
-        imageLink.target = "_blank";
+    let imageLink = document.createElement('a');
+    imageLink.href = `https://${articleLang}.wikipedia.org`;
+    imageLink.target = "_blank";
 
-            let image = document.createElement('img');
-            
-            image.src = chrome.runtime.getURL("images/wikipedia-logo.png");
-            image.style.position = "absolute";
-            image.style.top = "35px";
-            image.style.right = "25px";
-            image.style.width = "30px";
-            image.style.height = "30px";
+    let image = document.createElement('img');
+    image.src = chrome.runtime.getURL("images/wikipedia-logo.png");
+    image.style.position = "absolute";
+    image.style.top = "35px";
+    image.style.right = "25px";
+    image.style.width = "30px";
+    image.style.height = "30px";
 
-            imageLink.appendChild(image);
+    imageLink.appendChild(image);
+    modal.appendChild(imageLink);
 
-        modal.appendChild(imageLink);
-        
-        if(window.location.hostname.slice(-13) != "wikipedia.org"){
-            let lineBreak = document.createElement('hr')
-            lineBreak.style = "color: rgba(0,0,0,0.2); border: 0.1px solid rgba(0,0,0,0.2); border-radius: 20%; padding: 0px; margin: 0px; margin-top:20px; margin-bottom: 10px;";
-            modal.appendChild(lineBreak);
-        }
+    if (window.location.hostname.slice(-13) != "wikipedia.org") {
+        let lineBreak = document.createElement('hr')
+        lineBreak.style = "color: rgba(0,0,0,0.2); border: 0.1px solid rgba(0,0,0,0.2); border-radius: 20%; padding: 0px; margin: 0px; margin-top:20px; margin-bottom: 10px;";
+        modal.appendChild(lineBreak);
+    }
 
-        let article = document.createElement('p');
-        
+    let article = document.createElement('p');
+    if (articleExtract.length == 0) {
+        article.innerHTML = articleLang == 'ja' ? 'ウィキペディアには現在この名前の項目はありません。' : 'Wikipedia does not have an article with this exact name';
+    } else {
         article.innerHTML = articleExtract;
-        article.style = "font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; " + 
-                        "line-height: 26px; font-size: 16px; color: black; padding: 0px; margin: 0px;";
+    }
+    article.style = "font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; " +
+        "line-height: 26px; font-size: 16px; color: black; padding: 0px; margin: 0px;";
 
-        modal.appendChild(article);
+    modal.appendChild(article);
 
-        const link = `https://en.wikipedia.org/wiki/${articleTitle}`;
-        
-        let articleLink = document.createElement('a');
-        articleLink.innerHTML = "Read more";
-        articleLink.href = link;
-        articleLink.title = link;
-        articleLink.target = "_blank";
-        articleLink.style = "color: gray; float: right; font-size: 12px";
+    const link = `https://${articleLang}.wikipedia.org/wiki/${articleTitle}`;
 
-        modal.appendChild(articleLink);
+    let articleLink = document.createElement('a');
+    articleLink.href = link;
+    articleLink.title = link;
+    articleLink.innerHTML = articleLang == 'ja' ? '続きを読む' : 'Read more';
+    articleLink.target = "_blank";
+    articleLink.style = "color: gray; float: right; font-size: 12px";
+
+    modal.appendChild(articleLink);
 
     document.body.appendChild(modal);
 }
